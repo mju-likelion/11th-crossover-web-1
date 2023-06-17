7b018d0f0cac90fe4328bd302cd8219d4afea8bd
gyuhyuk <bruno3@naver.com>
2023-06-17 14:43:05 +0900

    GlobalStyle 세팅

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
-import logo from './logo.svg';
-import './App.css';
-
+import logo from "./logo.svg";
+import "./App.css";
+import "../src/styles/GlobalStyle";
 function App() {
   return (
     <div className="App">
